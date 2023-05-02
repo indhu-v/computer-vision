37eb29c430ff6c39f86ae87a0b119760835a6dd3
indhu-v <113018637+indhu-v@users.noreply.github.com>
2023-05-01 22:34:11 -0700

    Add files via upload

---
+++ rotate_image.py
@@ -0,0 +1,18 @@
+#image rotation
+import cv2
+
+
+path = r"C:/Users/INDHU/Downloads/pG07kr.jpg"
+
+
+src = cv2.imread(path)
+
+
+window_name = 'Image'
+
+
+image = cv2.rotate(src, cv2.ROTATE_90_CLOCKWISE)
+
+
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
